bbbacb0a0559271fdb30e22de424d9a6e1f4d803
Rosario Villarroel <ada_hack08@hackwomen.dev>
2023-04-22 23:48:40 -0400

    moviendo componente ListaLugares a una carpeta

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes,Route } from "react-router-dom";
 import "./App.css";
 import FormSecurePlace from "./components/FormSecurePlace/FormSecurePlace";
-import ListaLugares from "./components/ListaLugares";
+import ListaLugares from "./components/ListaLugares/ListaLugares";
 
 
 function App() {
