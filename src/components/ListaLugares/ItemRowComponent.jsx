f26fa001e672cdb14be9dd3b645123787a10d88b
KarlaAguila <ada_hack35@hackwomen.dev>
2023-04-23 18:12:27 -0600

    Conectando API en lista de lugares seguros

---
+++ src/components/ListaLugares/ItemRowComponent.jsx
@@ -0,0 +1,62 @@
+import PropTypes from "prop-types";
+import React from 'react';
+import Brightness1Icon from '@mui/icons-material/Brightness1'; 
+import LocationOnIcon from '@mui/icons-material/LocationOn'; 
+import CancelTwoToneIcon from '@mui/icons-material/CancelTwoTone'; 
+import StarIcon from '@mui/icons-material/Star'; 
+import './ListaLugares.css' 
+
+export default function ItemRowComponent({place}) {
+    
+    const lugarSeguro = {
+        /* name: "Cafe Starbucks",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industri. Lorem ipsum has been the industry’s standars dummy text ever since the 1500s", */
+
+        createdBy: "Rosario V",
+        creationDate: "6 Marzo 2023",
+        likes: 57,
+        /* address: "Av. Velarde, Santa Cruz de la Sierra" */
+    };
+
+    return (
+        <div className='item-row'>
+            <div className="item-image-column" >
+                <h2>Image</h2>
+            </div>
+            <div className="item-desc-column" >
+                <h2 className='ml-10'>{place.name}</h2>
+                <div>
+                    <div className='item-creation-cnt'>
+                        <p className='flex-auto item-creator-label m-0'>{lugarSeguro.createdBy}</p>
+                        <p className='flex-auto border-left m-0'>{lugarSeguro.creationDate}</p>
+                        <p className='flex-auto border-left m-0'>{lugarSeguro.likes} likes</p>
+                    </div>
+                    <div style={{ width: '60%'}}> </div>
+                </div>
+                <p className='ml-10'>Descripción: {place.description}</p>
+                <div className='btn-location ml-10'>
+                    <LocationOnIcon className='dp-inline-block location-icon'/>    
+                    <p className='btn-location dp-inline-block'> {`${place.address.state}, ${place.address.city}`}</p>
+                </div>
+            </div>
+            <div className="item-menu-column" >
+                <div className='btn-option'>
+                    <StarIcon />
+                    <p className='text-option'>Me Gusta</p>
+                </div>
+                <div className='btn-option'>
+                    <Brightness1Icon />
+                    <p className='text-option'>Editar</p>
+                </div>
+                <div className='btn-option'>
+                    <CancelTwoToneIcon className='cancel-icon'/>
+                    <p className='text-option'>Eliminar</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+ItemRowComponent.propTypes={
+    place:PropTypes.shape([]).isRequired,
+}
--- src/components/ListaLugares/ListaLugares.jsx
+++ src/components/ListaLugares/ListaLugares.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import './ListaLugares.css'
-import Brightness1Icon from '@mui/icons-material/Brightness1';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import CancelTwoToneIcon from '@mui/icons-material/CancelTwoTone';
-import StarIcon from '@mui/icons-material/Star';
+import { useEffect, useState } from 'react';
+import { getPlaces } from '../../api/places';
+import './ListaLugares.css' 
+import ItemRowComponent from './ItemRowComponent';
 
 export default function ListaLugares() {
+    const [places, setPlaces]=useState([])
+    const callGetPlaces=async() => {
+        const response = await getPlaces();
+        console.log(response);
+        if(response.status!==201){
+            console.log("Error al consultar los datos");
+            return
+        }
 
-    
-
-    const lugarSeguro = {
-        name: "Cafe Starbuck",
-        description: "Lorem Ipsum  is simply dummy text of the printing and typisetting industri. Lorem ipsum has been the industry’s standars dummy text ever since the 1500s",
-        createdBy: "Rosario V",
-        creationDate: "6 Marzo 2023",
-        likes: 57,
-        address: "Av. Velarde, Santa Cruz de la Sierra"
+        setPlaces(response.data)
     }
+
+    useEffect(()=>{
+        callGetPlaces();
+    }, [])
 
     return (
         <div className='list-container'>
@@ -34,42 +36,10 @@ export default function ListaLugares() {
                     No Seguros
                 </div>
             </div>
-            <div className="item-row">
-                <div className="item-image-column" >
-                    <h2>Image</h2>
-                </div>
-                    <div className="item-desc-column" >
-                    <h2 className='ml-10'>{lugarSeguro.name}</h2>
-                    <div>
-                        <div className='item-creation-cnt'>
-                            <p className='flex-auto item-creator-label m-0'>{lugarSeguro.createdBy}</p>
-                            <p className='flex-auto border-left m-0'>{lugarSeguro.creationDate}</p>
-                            <p className='flex-auto border-left m-0'>{lugarSeguro.likes} likes</p>
-                        </div>
-                        <div style={{ width: '60%'}}> </div>
-                    </div>
-                    <p className='ml-10'>Descripcion: {lugarSeguro.description}</p>
-                    <div className='btn-location ml-10'>
-                        <LocationOnIcon className='dp-inline-block location-icon'/>    
-                        <p className='btn-location dp-inline-block'> {lugarSeguro.address}</p>
-                    </div>
-                    
-                </div>
-                <div className="item-menu-column" >
-                    <div className='btn-option'>
-                        <StarIcon />
-                        <p className='text-option'>Me Gusta</p>
-                    </div>
-                    <div className='btn-option'>
-                        <Brightness1Icon />
-                        <p className='text-option'>Editar</p>
-                    </div>
-                    <div className='btn-option'>
-                        <CancelTwoToneIcon className='cancel-icon'/>
-                        <p className='text-option'>Eliminar</p>
-                    </div>
-                </div>
-            </div>
+                {places.map((place) => (
+                    <ItemRowComponent place={place} key={place.id} /> 
+                ))}
+                
         </div>
     )
 }
